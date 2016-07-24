2d35340473fe1369bd992bfec72af6bdf5c1a0a5
TEAMvirus <virusteam@doctor.com>
2016-07-24 06:36:01 +0430

    Create id.lua

---
+++ plugins/id.lua
@@ -0,0 +1,14 @@
+do
+function run(msg, matches)
+  return "👾ViRuS™🗨# ??👩👦GrouP ID : "..msg.from.id.."\n🔺 # 👩👩👦👦GrouP NamE: "..msg.to.title.."\n1⃣ # 🗣YouRe NamE: "..(msg.from.first_name or '').."\n2⃣ # ✍FirsT NamE : "..(msg.from.first_name or '').."\n3⃣ # ✍LasT NamE : "..(msg.from.last_name or '').."\n4⃣ # 🎫ID : "..msg.from.id.."\n5⃣ # 💥UseR NamE : @"..(msg.from.username or '').."\n6⃣ # 📞PhonE NumbeR : +"..(msg.from.phone or '')
+end
+return {
+  description = "info", 
+  usage = "info",
+  patterns = {
+    "^[!/#]([Ii][Dd])$",
+    "^[Ii][Dd]$",
+  },
+  run = run
+}
+end
