2b34bdf57e374a029c7e1fe7b017348b22801ff0
TEAMvirus <virusteam@doctor.com>
2016-07-24 05:34:55 +0430

    Update Signal_bot.lua

--- bot/Signal_bot.lua
+++ bot/Signal_bot.lua
@@ -264,7 +264,8 @@ function create_config( )
     "Sticker",
     "Photo",
     "Aparat",
-    "InvPouria",
+    "invamour",
+    "Invsaman",
     "Del_Gban",
     "Date",
     "Badwords",
@@ -274,7 +275,7 @@ function create_config( )
     "Caption",
     "Payamresan"
     },
-    sudo_users = {175636120,214795464,198794027,180252366},
+    sudo_users = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[👑 Signal Bot 👑
 An advanced administration bot based on TG-CLI written in Lua
